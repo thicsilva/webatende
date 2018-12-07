a412d0c8c852a57aa36af63232a99127ae47157c
Thiago Silva <thicsilva@yahoo.com.br>
2018-12-07 13:27:15 -0200

    Aplicando tema StarAdmin

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/*.js', 'public/js')
+   .extract(['jquery', 'popper.js', 'bootstrap'])
    .sass('resources/sass/app.scss', 'public/css');
+if (mix.inProduction()){
+    mix.version();
+}
